a17fdaefb21790b353859db06c1b54f1d24ca07a
yukang.wang <yukang.wang@mthreads.com>
2024-08-16 15:18:31 +0800

    [debug]change scale from projection matrix to model matrix
    Signed-off-by: yukang.wang <yukang.wang@mthreads.com>

--- modules/core/exec/drawFrame.cpp
+++ modules/core/exec/drawFrame.cpp
@@ -80,9 +80,9 @@ void Application::updateUniformBuffer(){
     
     UniformBufferObject ubo{};
     ubo.model = glm::rotate(glm::mat4(1.0f), (currentTime - startTime) * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    ubo.model = glm::scale(ubo.model, glm::vec3(1.0f, 1.0f, (float)swapChainExtent.height/MODEL_HEIGHT));
     ubo.view = camera.getViewMatrix();
     ubo.proj = glm::perspective(glm::radians(camera.zoom), static_cast<float>(swapChainExtent.width / swapChainExtent.height), 0.1f, 10.0f);
-    ubo.proj = glm::scale(ubo.proj, glm::vec3((float)MODEL_WIDTH/swapChainExtent.width, (float)MODEL_HEIGHT/swapChainExtent.height, 1.0f));
     ubo.proj[1][1] *= -1;
 
     memcpy(uniformBuffersMapped[currentFrame], &ubo, sizeof(ubo));
