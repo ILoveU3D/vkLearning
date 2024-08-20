5142f684bac944f218ef9ddedc154a8b71a5c720
yukang.wang <yukang.wang@mthreads.com>
2024-08-20 17:00:26 +0800

    [review]change stype of Cmake
    Signed-off-by: yukang.wang <yukang.wang@mthreads.com>

--- debug.sh
+++ debug.sh
@@ -2,30 +2,26 @@ compile(){
     cd shaders
     glslc shader.vert -o vert.spv
     glslc shader.frag -o frag.spv
-    cd ../build
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
-    make
+    cd ..
+    cmake -S . -B build
+    cmake --build build
 }
 
 run(){
     export DISPLAY=:0.0
-    gdb ./triangle
+    gdb ./build/triangle
 }
 
 case $1 in
     1)
         compile
-        cd ..
         ;;
     2)
-        cd build
         run
-        cd ..
         ;;
     3)
         compile
         run
-        cd ..
         ;;
     *)
         echo "this is 三角形程序运行脚本：（1：编译，2：运行，3：编译运行）"
--- run.sh
+++ run.sh
@@ -2,30 +2,26 @@ compile(){
     cd shaders
     glslc shader.vert -o vert.spv
     glslc shader.frag -o frag.spv
-    cd ../build
-    cmake ..
-    make
+    cd ..
+    cmake -S . -B build
+    cmake --build build
 }
 
 run(){
     export DISPLAY=:0.0
-    ./triangle
+    ./build/triangle
 }
 
 case $1 in
     1)
         compile
-        cd ..
         ;;
     2)
-        cd build
         run
-        cd ..
         ;;
     3)
         compile
         run
-        cd ..
         ;;
     *)
         echo "this is 程序运行脚本：（1：编译，2：运行，3：编译运行）"
